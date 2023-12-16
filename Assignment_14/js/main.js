eb607d9a3e96d5bb10c757a11dc0b97ba31f5597
Trevan Morin <tm194639@umconnect.umt.edu>
2023-12-15 22:39:28 -0700

    f

--- Assignment_14/js/main.js
+++ Assignment_14/js/main.js
@@ -1,13 +1,49 @@
-// Function to draw a red border around the canvas
-function drawBorder() {
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
+// main.js
 
-    // Draw a red border around the canvas
-    ctx.strokeStyle = 'red';
+// Get the canvas element
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+// Draw a comic book-style border around an image
+function drawComicBorder() {
+    // Clear the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Outer border
+    ctx.strokeStyle = "black";
     ctx.lineWidth = 10;
-    ctx.strokeRect(5, 5, c.width - 10, c.height - 10);
+    ctx.strokeRect(5, 5, canvas.width - 10, canvas.height - 10);
+
+    // Inner border
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 5;
+    ctx.strokeRect(15, 15, canvas.width - 30, canvas.height - 30);
+
+    // Red line around the border
+    ctx.strokeStyle = "red";
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.moveTo(5, 5);
+    ctx.lineTo(canvas.width - 5, 5);
+    ctx.lineTo(canvas.width - 5, canvas.height - 5);
+    ctx.lineTo(5, canvas.height - 5);
+    ctx.closePath();
+    ctx.stroke();
+
+    // Dots on the corners
+    ctx.fillStyle = "black";
+    ctx.fillRect(5, 5, 20, 20);
+    ctx.fillRect(canvas.width - 25, 5, 20, 20);
+    ctx.fillRect(5, canvas.height - 25, 20, 20);
+    ctx.fillRect(canvas.width - 25, canvas.height - 25, 20, 20);
 }
 
-// Call the drawBorder function when the page loads
-window.onload = drawBorder;
+// Redraw the comic border when the window is resized
+window.addEventListener("resize", function () {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    drawComicBorder();
+});
+
+// Initial draw
+drawComicBorder();
