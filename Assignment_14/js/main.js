81d3d3c800460cde80d275bd50dd7f137c2cb8c6
Trevan Morin <tm194639@umconnect.umt.edu>
2023-12-15 22:50:48 -0700

    f

--- Assignment_14/js/main.js
+++ Assignment_14/js/main.js
@@ -1,24 +1,35 @@
-// js/main.js
+// main.js
 
-// Add your JavaScript code for canvas interactions, animations, etc.
+// Get the canvas element
+var canvas = document.getElementById("comicsCanvas");
+var ctx = canvas.getContext("2d");
 
-// Example: Simple comic book panel effect using HTML5 canvas
-document.addEventListener('DOMContentLoaded', function () {
-    const canvas = document.getElementById('comicPanel');
-    if (canvas) {
-        const ctx = canvas.getContext('2d');
+// Draw the outlines of comic books
+function drawComicOutlines() {
+    // Clear the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-        // Draw a simple border around the canvas
-        ctx.strokeStyle = 'black';
-        ctx.strokeRect(0, 0, canvas.width, canvas.height);
+    // Comic book outlines
+    drawComicOutline(50, 50, 200, 300);  // Example: x, y, width, height
+    drawComicOutline(300, 50, 200, 300);
+    drawComicOutline(550, 50, 200, 300);
 
-        // Example: Draw a speech bubble
-        ctx.beginPath();
-        ctx.moveTo(50, 50);
-        ctx.lineTo(100, 0);
-        ctx.lineTo(150, 50);
-        ctx.stroke();
-        ctx.font = '14px Arial';
-        ctx.fillText('Hello, comic world!', 55, 25);
-    }
+    // You can add more outlines as needed
+}
+
+// Function to draw a single comic book outline
+function drawComicOutline(x, y, width, height) {
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 5;
+    ctx.strokeRect(x, y, width, height);
+}
+
+// Redraw the comic outlines when the window is resized
+window.addEventListener("resize", function () {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    drawComicOutlines();
 });
+
+// Initial draw
+drawComicOutlines();
