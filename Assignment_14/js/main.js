31420e1c2e285d4ee749c612e7c3493aa303c9dc
Trevan Morin <tm194639@umconnect.umt.edu>
2023-12-15 22:53:49 -0700

    f

--- Assignment_14/js/main.js
+++ Assignment_14/js/main.js
@@ -4,32 +4,45 @@
 var canvas = document.getElementById("comicsCanvas");
 var ctx = canvas.getContext("2d");
 
-// Draw the outlines of comic books
-function drawComicOutlines() {
-    // Clear the canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+// Function to draw a comic book-style border with a question mark in the center
+function drawComicBorder(x, y) {
+    // Outer border
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 10;
+    ctx.strokeRect(x, y, 160, 220);
 
-    // Comic book outlines
-    drawComicOutline(50, 50, 200, 300);  // Example: x, y, width, height
-    drawComicOutline(300, 50, 200, 300);
-    drawComicOutline(550, 50, 200, 300);
-
-    // You can add more outlines as needed
-}
-
-// Function to draw a single comic book outline
-function drawComicOutline(x, y, width, height) {
+    // Inner border
     ctx.strokeStyle = "black";
     ctx.lineWidth = 5;
-    ctx.strokeRect(x, y, width, height);
+    ctx.strokeRect(x + 10, y + 10, 140, 200);
+
+    // Red line around the border
+    ctx.strokeStyle = "red";
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(x + 160, y);
+    ctx.lineTo(x + 160, y + 220);
+    ctx.lineTo(x, y + 220);
+    ctx.closePath();
+    ctx.stroke();
+
+    // Dots on the corners
+    ctx.fillStyle = "black";
+    ctx.fillRect(x, y, 20, 20);
+    ctx.fillRect(x + 140, y, 20, 20);
+    ctx.fillRect(x, y + 200, 20, 20);
+    ctx.fillRect(x + 140, y + 200, 20, 20);
+
+    // Question mark in the center
+    ctx.font = '40pt Arial';
+    ctx.fillStyle = 'black';
+    ctx.fillText('?', x + 70, y + 120);
 }
 
-// Redraw the comic outlines when the window is resized
-window.addEventListener("resize", function () {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    drawComicOutlines();
-});
-
-// Initial draw
-drawComicOutlines();
+// Draw five comic book borders
+drawComicBorder(10, 10);
+drawComicBorder(180, 10);
+drawComicBorder(350, 10);
+drawComicBorder(520, 10);
+drawComicBorder(690, 10);
